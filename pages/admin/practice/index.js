7159d88a6cf2f0ae57187c0a8e2f434ef4746dd1
joko19 <jokoslamet459@gmail.com>
2022-02-15 14:49:43 +0700

    change useeffect

--- pages/admin/practice/index.js
+++ pages/admin/practice/index.js
@@ -43,15 +43,12 @@ export default function Index() {
       .catch((err) => {
         // console.log(err)
       })
-  }
-  useEffect(() => {
-    getData(search, type, status, limit, page)
-  }, [])
-
-  useEffect(async () => {
     await apiExam.allType()
       .then((res) => setTypePractice(res.data.data))
       .catch((err) => console.log(err))
+  }
+  useEffect(() => {
+    getData(search, type, status, limit, page)
   }, [])
 
 
